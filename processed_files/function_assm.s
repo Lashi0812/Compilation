b5c2e27952857195077a6877e6a6e3b0ad4d41f7
Lashi <lashi0812@gmail.com>
2024-01-24 17:34:32 +0000

    Compile and Linking

---
+++ processed_files/function_assm.s
@@ -0,0 +1,87 @@
+	.file	"function.c"
+	.text
+	.globl	nCompletionStatus
+	.bss
+	.align 4
+	.type	nCompletionStatus, @object
+	.size	nCompletionStatus, 4
+nCompletionStatus:
+	.zero	4
+	.text
+	.globl	add
+	.type	add, @function
+add:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movss	%xmm0, -20(%rbp)
+	movss	%xmm1, -24(%rbp)
+	movss	-20(%rbp), %xmm0
+	addss	-24(%rbp), %xmm0
+	movss	%xmm0, -4(%rbp)
+	movss	-4(%rbp), %xmm0
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add, .-add
+	.globl	add_and_multiply
+	.type	add_and_multiply, @function
+add_and_multiply:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$24, %rsp
+	movss	%xmm0, -20(%rbp)
+	movss	%xmm1, -24(%rbp)
+	movss	-24(%rbp), %xmm0
+	movl	-20(%rbp), %eax
+	movaps	%xmm0, %xmm1
+	movd	%eax, %xmm0
+	call	add
+	movd	%xmm0, %eax
+	movl	%eax, -4(%rbp)
+	movss	-4(%rbp), %xmm1
+	movss	.LC0(%rip), %xmm0
+	mulss	%xmm1, %xmm0
+	movss	%xmm0, -4(%rbp)
+	movss	-4(%rbp), %xmm0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	add_and_multiply, .-add_and_multiply
+	.section	.rodata
+	.align 4
+.LC0:
+	.long	1077936128
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
